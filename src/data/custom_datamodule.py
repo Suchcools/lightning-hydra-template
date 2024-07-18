007e22433473d14d121ff419617be1d166bf59f7
Suchcools <1175721708@qq.com>
2024-07-18 10:54:14 +0800

    add custom

---
+++ src/data/custom_datamodule.py
@@ -0,0 +1,217 @@
+from typing import Any, Dict, Optional, Tuple
+import os
+import pandas as pd
+import torch
+from PIL import Image
+from lightning import LightningDataModule
+from torch.utils.data import ConcatDataset, DataLoader, Dataset, random_split
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
+
+
+def find_image_file(source_path, file_lst):
+    """
+    递归寻找 文件夹以及子目录的 图片文件。
+    :param source_path: 源文件夹路径
+    :param file_lst: 输出 文件路径列表
+    :return:
+    """
+    image_ext = ['.jpg']
+    for dir_or_file in os.listdir(source_path):
+        file_path = os.path.join(source_path, dir_or_file)
+        if os.path.isfile(file_path):  # 判断是否为文件
+            file_name_ext = os.path.splitext(
+                os.path.basename(file_path))  # 文件名与后缀
+            if len(file_name_ext) < 2:
+                continue
+            if file_name_ext[1] in image_ext:  # 后缀在后缀列表中
+                file_lst.append(file_path)
+            else:
+                continue
+        elif os.path.isdir(file_path):  # 如果是个dir，则再次调用此函数，传入当前目录，递归处理。
+            find_image_file(file_path, file_lst)
+        else:
+            print('文件夹没有环境' + os.path.basename(file_path))
+
+
+class CustomDataset(Dataset):
+    def __init__(self, file_path, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        env_path_list = []
+        find_image_file(file_path, env_path_list)
+        label = [x.split('/')[-2] for x in env_path_list]
+        rawdata = pd.DataFrame([env_path_list, label],
+                               index=['path', 'label']).T
+        class_labels = rawdata.label.value_counts().index.tolist()
+        self.label = rawdata.label.apply(lambda x: class_labels.index(x)).values
+        self.data_frame = rawdata
+        self.root_dir = file_path
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.data_frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir, self.data_frame.iloc[idx, 0])
+        image = Image.open(img_name)
+        label = self.label[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+class CustomDataModule(LightningDataModule):
+
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        train_val_test_split: Tuple[int, int, int] = (55_000, 5_000, 10_000),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ) -> None:
+        """Initialize a `MNISTDataModule`.
+
+        :param data_dir: The data directory. Defaults to `"data/"`.
+        :param train_val_test_split: The train, validation and test split. Defaults to `(55_000, 5_000, 10_000)`.
+        :param batch_size: The batch size. Defaults to `64`.
+        :param num_workers: The number of workers. Defaults to `0`.
+        :param pin_memory: Whether to pin memory. Defaults to `False`.
+        """
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        # data transformations
+        self.transforms = transforms.Compose(
+            [transforms.ToTensor(), transforms.Normalize(
+                (0.1307,), (0.3081,)), transforms.Resize((256, 256))]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+        self.batch_size_per_device = batch_size
+
+    @property
+    def num_classes(self) -> int:
+        """Get the number of classes.
+
+        :return: The number of MNIST classes (10).
+        """
+        return 4
+
+    def prepare_data(self) -> None:
+        # Download data if needed.
+        pass
+
+    def setup(self, stage: Optional[str] = None) -> None:
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+
+        This method is called by Lightning before `trainer.fit()`, `trainer.validate()`, `trainer.test()`, and
+        `trainer.predict()`, so be careful not to execute things like random split twice! Also, it is called after
+        `self.prepare_data()` and there is a barrier in between which ensures that all the processes proceed to
+        `self.setup()` once the data is prepared and available for use.
+
+        :param stage: The stage to setup. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`. Defaults to ``None``.
+        """
+        # Divide batch size by the number of devices.
+        if self.trainer is not None:
+            if self.hparams.batch_size % self.trainer.world_size != 0:
+                raise RuntimeError(
+                    f"Batch size ({self.hparams.batch_size}) is not divisible by the number of devices ({self.trainer.world_size})."
+                )
+            self.batch_size_per_device = self.hparams.batch_size // self.trainer.world_size
+
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_val and not self.data_test:
+            dataset = CustomDataset(
+                file_path=self.hparams.data_dir, transform=self.transforms)
+            train_size = int(0.8 * len(dataset))
+            test_size = len(dataset) - train_size
+
+            self.data_train, self.data_test = random_split(
+                dataset=dataset,
+                lengths=[train_size, test_size],
+                generator=torch.Generator().manual_seed(42),
+            )
+            self.data_val = self.data_test
+            
+    def train_dataloader(self) -> DataLoader[Any]:
+        """Create and return the train dataloader.
+
+        :return: The train dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self) -> DataLoader[Any]:
+        """Create and return the validation dataloader.
+
+        :return: The validation dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self) -> DataLoader[Any]:
+        """Create and return the test dataloader.
+
+        :return: The test dataloader.
+        """
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.batch_size_per_device,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None) -> None:
+        """Lightning hook for cleaning up after `trainer.fit()`, `trainer.validate()`,
+        `trainer.test()`, and `trainer.predict()`.
+
+        :param stage: The stage being torn down. Either `"fit"`, `"validate"`, `"test"`, or `"predict"`.
+            Defaults to ``None``.
+        """
+        pass
+
+    def state_dict(self) -> Dict[Any, Any]:
+        """Called when saving a checkpoint. Implement to generate and save the datamodule state.
+
+        :return: A dictionary containing the datamodule state that you want to save.
+        """
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        """Called when loading a checkpoint. Implement to reload datamodule state given datamodule
+        `state_dict()`.
+
+        :param state_dict: The datamodule state returned by `self.state_dict()`.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    _ = CustomDataModule()
--- src/train.py
+++ src/train.py
@@ -1,5 +1,14 @@
 from typing import Any, Dict, List, Optional, Tuple
 
+# import debugpy
+# try:
+#     # 5678 is the default attach port in the VS Code debug configurations. Unless a host and port are specified, host defaults to 127.0.0.1
+#     debugpy.listen(("localhost", 9501))
+#     print("Waiting for debugger attach")
+#     debugpy.wait_for_client()
+# except Exception as e:
+#     pass
+
 import hydra
 import lightning as L
 import rootutils
